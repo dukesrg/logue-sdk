fac0a96e5bc4e728041e0a1ef9ed36123c5d1f77
Oleg Burdaev <dukesrg@gmail.com>
2025-10-17 22:29:53 +0200

    Peak detect fix (WiP)

--- platform/inc/peak_detect.h
+++ platform/inc/peak_detect.h
@@ -3,7 +3,7 @@
  *
  *  Peak detector
  *
- *  2024 (c) Oleg Burdaev
+ *  2024-2025 (c) Oleg Burdaev
  *  mailto: dukesrg@gmail.com
  */
 
@@ -28,25 +28,25 @@ struct peak_detect {
 
   peak_detect(void (*peak_high)(), void (*peak_low)()) : peak_high_callback{peak_high}, peak_low_callback{peak_low} {}
 
-  inline __attribute__((optimize("Ofast"), always_inline))  
+  inline __attribute__((optimize("Ofast"), always_inline))
   void set_threshold(float threshold_level_db) {
     threshold_level = fasterdbampf(threshold_level_db);
   }
 
-  inline __attribute__((optimize("Ofast"), always_inline))  
+  inline __attribute__((optimize("Ofast"), always_inline))
   void set_time(float time_s) {
     time_counter = 48000 * time_s;
   }
 
-  inline __attribute__((optimize("Ofast"), always_inline))  
+  inline __attribute__((optimize("Ofast"), always_inline))
   void process(const float * in, uint32_t frames, uint32_t channels, uint32_t channel_mask) {
     const float * __restrict in_p = in;
     const float * in_e = in_p + frames * channels;
     bool high_pending = false;
     for (; in_p != in_e; in_p += channels) {
       sample_counter++;
-      for (uint32_t i = 1 << channels; i >>= 1; i >>= 1) {
-        if (i & channel_mask && abs(*in_p) > threshold_level) {
+      for (uint32_t i = 0; i < channels; i++) {
+        if (channel_mask >> i & 1 && abs(in_p[i]) > threshold_level) {
           high_pending = true;
           sample_counter = 0;
           break;
